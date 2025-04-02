8775e16b756fb826b781c3258c51210b9d66dac8
MYABUKI <momoe-yabuki@cresco-jcube.co.jp>
2025-04-02 18:08:34 +0900

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import reactLogo from "./assets/react.svg";
+import reactLogo from "./assets/HeshikiriHasebe.webp";
 import "./App.css";
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={reactLogo} className="logo react" alt="React logo" />
 
-        <h1>Hello from Amplify</h1>
+        <h1>Ç÷Çµêÿí∑íJïî</h1>
       </header>
     </div>
   );
